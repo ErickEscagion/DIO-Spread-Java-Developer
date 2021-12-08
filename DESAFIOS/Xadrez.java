bc46682c18d7011443e81a6b4ca609f3ded6e27a
Erick Escagion <erickeescagion@gmail.com>
2021-12-08 17:26:02 -0300

    Desafio 1

---
+++ DESAFIOS/Xadrez.java
@@ -0,0 +1,15 @@
+package DESAFIOS;
+
+import java.util.Scanner;
+public class Xadrez {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int L = sc.nextInt();
+		int C = sc.nextInt();
+		if ((L+C)%2==0)
+			System.out.println("1");
+		else                                      
+			System.out.println("0");
+		sc.close();
+	}
+}
