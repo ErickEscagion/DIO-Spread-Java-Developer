35bebc2d98115d60fdc77947462dceb23a328f47
Erick Escagion <erickeescagion@gmail.com>
2021-12-14 11:17:49 -0300

    create app class

--- DesafioPOO/BOOTCAMP/src/App.java
+++ DesafioPOO/BOOTCAMP/src/App.java
@@ -1,5 +1,38 @@
+import java.time.LocalDate;
+
+import allClass.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Course course = new Course();
+        course.setDescription("Description course");
+        course.setTitle("Title course");
+        course.setWorkload(40);
+
+        Mentoring mentoring = new Mentoring();
+        mentoring.setTitle("Title mentoring");
+        mentoring.setDescription("Description mentoring");
+        mentoring.setDate(LocalDate.now());
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setName("Bootcamp name");
+        bootcamp.setDescription("Description bootcamp");
+        bootcamp.getContents().add(course);
+        bootcamp.getContents().add(mentoring);
+
+        Dev devErick = new Dev();
+        devErick.setName("Erick");
+        devErick.signUpBootcamp(bootcamp);
+
+        Dev dev2 = new Dev();
+        dev2.setName("dev 2");
+        dev2.signUpBootcamp(bootcamp);
+        dev2.progress();
+
+
+        System.out.println(devErick.getSignedContents());
+        
+        System.out.println(dev2.getCompletedContents());
     }
 }
