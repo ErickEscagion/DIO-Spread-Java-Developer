3cb81f7ff79b740ff6d31163d47d8e23c6d7ec1e
Erick Escagion <erickeescagion@gmail.com>
2021-12-14 11:16:28 -0300

    create content class

---
+++ DesafioPOO/BOOTCAMP/src/allClass/Content.java
@@ -0,0 +1,29 @@
+package allClass;
+
+public abstract class Content {
+
+    protected static final double XP_PADRAO = 10d;
+
+    private String title;
+    private String description;
+
+    public abstract double calculateXp();
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    
+}
